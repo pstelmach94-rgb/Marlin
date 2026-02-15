6b04e182a9552dc94a2f09991dc78b1623ac58a5
pstelmach94-rgb <pstelmach94@gmail.com>
2026-02-15 03:50:19 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -68,7 +68,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_CREALITY_V4_2_2
+  #define MOTHERBOARD BOARD_CREALITY_V422
 #endif
 
 // @section serial
